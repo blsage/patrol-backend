9b0720d5dc5b78319990629bc2782e762166733a
blsage <ben@sage.me>
2024-11-26 16:02:47 -0800

    Added procfile

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use('/auth', authRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
