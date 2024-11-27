2fdffff111db14620796fe9bfd040ef049c50522
blsage <ben@sage.me>
2024-11-26 18:30:48 -0800

    Updated build

--- dist/server.js
+++ dist/server.js
@@ -8,12 +8,9 @@ const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const PORT = 3000;
 app.use(express_1.default.json());
 app.use('/auth', authRoutes_1.default);
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
