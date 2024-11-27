d4736c695573afa7fbbf123bc0830e70c4f9c7f8
blsage <ben@sage.me>
2024-11-26 18:33:25 -0800

    Twilio errors

--- src/services/smsService.ts
+++ src/services/smsService.ts
@@ -7,15 +7,25 @@ const verifyServiceSid = process.env.TWILIO_VERIFY_SERVICE_SID!;
 const twilioClient = new Twilio(accountSid, authToken);
 
 export const sendVerificationCode = async (phoneNumber: string) => {
-    await twilioClient.verify.v2
-        .services(verifyServiceSid)
-        .verifications.create({ to: phoneNumber, channel: 'sms' });
+    try {
+        await twilioClient.verify.v2
+            .services(verifyServiceSid)
+            .verifications.create({ to: phoneNumber, channel: 'sms' });
+    } catch (error) {
+        console.error('Twilio Error:', error);
+        throw new Error('Failed to send verification code.');
+    }
 };
 
 export const verifyCode = async (phoneNumber: string, code: string) => {
-    const verificationCheck = await twilioClient.verify.v2
-        .services(verifyServiceSid)
-        .verificationChecks.create({ to: phoneNumber, code });
+    try {
+        const verificationCheck = await twilioClient.verify.v2
+            .services(verifyServiceSid)
+            .verificationChecks.create({ to: phoneNumber, code });
 
-    return verificationCheck.status === 'approved';
+        return verificationCheck.status === 'approved';
+    } catch (error) {
+        console.error('Twilio Error:', error);
+        throw new Error('Failed to verify code.');
+    }
 };
